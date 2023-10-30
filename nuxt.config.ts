c0fd14433446b6d6ea1d6f87341280191c7cee9b
Heaven Aldrico <aldrico.ffs@gmail.com>
2023-10-31 04:20:28 +0700

    chore: add modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,19 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+    devtools: { enabled: true },
+    css: ['~/assets/css/main.css'],
+    modules: [
+        '@nuxtjs/tailwindcss',
+        '@nuxtjs/google-fonts',
+        '@pinia/nuxt',
+        '@vueuse/nuxt',
+        'nuxt-icon',
+    ],
+    tailwindcss: {
+        exposeConfig: true,
+    },
+    googleFonts: {
+        families: {
+            Inter: true,
+        },
+    },
+});
