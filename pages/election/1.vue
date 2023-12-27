a221a07dd8f6abe42808f02ea37292749feb0b48
Heaven Aldrico <aldrico.ffs@gmail.com>
2023-12-28 01:53:08 +0700

    test camera

--- pages/election/1.vue
+++ pages/election/1.vue
@@ -94,7 +94,6 @@ const next = () => {
 
 const mobileCheck = () => {
     let check = false;
-    return true;
     (function (a) {
         if (
             /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
